c28b8ee337c765008eb0a63edb0be6b4a2c70ed2
kbeazer <kbeazer@fullsail.edu>
2013-10-02 20:03:01 -0400

    The Duel
    nex idex file added to Duel project

--- goal1_duel1/js/main.js
+++ goal1_duel1/js/main.js
@@ -2,12 +2,12 @@
 // 10/1/13
 // The Dual Assignment
 
-var leftFighter= 'SOULJAH BLESS';
-var rightFighter= 'SPLIT SOUL';
+var leftFighter= 'KABAL';
+var rightFighter= 'KRATOS';
 var leftHealth= 100;
 var rightHealth= 100;
-var leftDamage= 45;
-var rightDamage= 45;
+var leftDamage= 50;
+var rightDamage= 50;
 var round= 0;
 
 function kingFighter(){
@@ -26,21 +26,29 @@ function kingFighter(){
 
         if(result=== 'no winner'){
             round++;
-            alert(leftFighter+': '+leftHealth+'   ROUND '+round+' OVER   '+rightFighter+': '+rightHealth);
+            alert(leftFighter+': '+leftHealth+'   **Round '+round+' Over**   '+rightFighter+': '+rightHealth);
         }else{
             alert(result);
             break;
+        }if(round== 10 && leftHealth>rightHealth){
+            alert('Game Over '+leftFighter+' '+'Wins!!!')
+        }else if(round== 10 && leftHealth<rightHealth){
+            alert('Game Over '+rightFighter+' '+'Wins!!!')
+        }else if(round== 10 && leftHealth==rightHealth){
+            alert(result);
         }
 }
 
 function winnerSelector(){
     var result= 'no winner';
     if(leftHealth<1 && rightHealth<1){
-        result= 'Draw!!'
+        result= 'TKO!!'
+   }else if(round== 10 && leftHealth==rightHealth){
+        result= "It's a DRAW!!!"
    }else if(leftHealth<1){
-            result= rightFighter+' '+'wins!!!';
+        result= 'Game Over '+rightFighter+' '+'Wins!!!';
    }else if(rightHealth<1){
-                result= leftFighter+' '+'wins!!!'
+        result= 'Game Over '+leftFighter+' '+'Wins!!!'
             }
    return result;
     }
